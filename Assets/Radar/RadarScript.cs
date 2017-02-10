b9924e02f6deb415b9a3db4dcee93dc9e1bae1a6
Marcello <marcellocainelli@hotmail.com>
2017-02-10 11:24:27 -0200

    Jogo adicionado

---
+++ Assets/Radar/RadarScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RadarScript : MonoBehaviour {
+    public int vmax;
+    public Image moto;
+    public Image trem;
+    public Image bb8;
+    public Image mario;
+    public Image elza;
+    public Image minion;
+    public Image gameover;
+    public Rigidbody objectToMeasure;
+
+    void Start()
+    {
+        gameover.enabled = false;
+    }
+
+    public void OnTriggerEnter(Collider col)
+    {
+        double speed;
+        speed = objectToMeasure.velocity.magnitude * 3.6;
+        if (speed > vmax)
+        {
+            if (minion.enabled == true)
+                minion.enabled = false;
+            else if (elza.enabled == true)
+                elza.enabled = false;
+            else if (mario.enabled == true)
+                mario.enabled = false;
+            else if (bb8.enabled == true)
+                bb8.enabled = false;
+            else if (trem.enabled == true)
+                trem.enabled = false;
+            else if (moto.enabled == true)
+            {
+                moto.enabled = false;
+                gameover.enabled = true;
+                Time.timeScale = 0;
+            }
+        }
+    }
+}
